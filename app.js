f19b9c5148c62f5ff224d19da8132bcefe29214f
AmberCDH <kanarieamber@gmail.com>
2023-02-13 16:47:30 +0100

    Neo4j implemented

--- app.js
+++ app.js
@@ -1,40 +1,56 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const TrainerRoute = require('./Routes/Trainer')
-const PokemonRoute = require('./Routes/Pokemon')
-const ItemRoute = require('./Routes/Item')
-const RequestFriendshipRoute = require('./Routes/RequestFriendship')
+const TrainerRoute = require("./Routes/Trainer");
+const PokemonRoute = require("./Routes/Pokemon");
+const ItemRoute = require("./Routes/Item");
+const RequestFriendshipRoute = require("./Routes/RequestFriendship");
 
-const neo4j = require('neo4j-driver')
-const http = require('http');
-const express = require('express');
-const mongoose = require('mongoose');
-const mongoString = process.env.DATABASE_URL
-const port = process.env.PORT
+const neo4j = require("neo4j-driver");
+const http = require("http");
+const express = require("express");
+const mongoose = require("mongoose");
+const mongoString = process.env.DATABASE_URL;
+const port = process.env.PORT;
+
+const driver = neo4j.driver(
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(process.env.NEO4J_DB_NAME, process.env.NEO4J_PASSWORD)
+);
 
 mongoose.connect(mongoString);
-const database = mongoose.connection
+const database = mongoose.connection;
 
-database.on('error', (error) => {
-    console.log(error)
-})
+database.on("error", (error) => {
+  console.log(error);
+});
 
-database.once('connected', () => {
-    console.log('Database Connected');
-})
-
+database.once("connected", () => {
+  console.log("Database Connected");
+});
 
 const app = express();
+
+app.get("/get", async (req, res) => {
+  const session = driver.session();
+  const response = session.run(`MATCH (n) RETURN n`);
+
+  res.json({
+    status: "ok",
+    path: (await response)
+  });
+
+  await session.close()
+});
+
 app.use(express.json());
-app.use("/Item", ItemRoute)
-app.use("/Pokemon", PokemonRoute)
-app.use("/Trainer",TrainerRoute)
-app.use('/Request',RequestFriendshipRoute)
-app.use('/', function(req, res) {
-    res.send('NoSQL pokemon API works');
+app.use("/Item", ItemRoute);
+app.use("/Pokemon", PokemonRoute);
+app.use("/Trainer", TrainerRoute);
+app.use("/Request", RequestFriendshipRoute);
+app.use("/", function (req, res) {
+  res.send("NoSQL pokemon API works");
 });
 const server = http.createServer(app);
 
-
-server.listen(port);console.debug('localhost:' + port + '/');
-
+server.listen(port);
+console.debug("localhost:" + port + "/");
